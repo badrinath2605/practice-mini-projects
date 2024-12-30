f685291e3d4e7b527c4ca70ec2ef3c53c1126cf9
badrinath2605 <140091132+badrinath2605@users.noreply.github.com>
2024-12-30 23:51:34 +0530

    Delete miniproj-chess/help.py

--- miniproj-chess/help.py
+++
@@ -1,4 +0,0 @@
-help = '        <tr>/n            <td class="cell" id="2A"></td>/n            <td class="cell" id="2B"></td>/n            <td class="cell" id="2C"></td>/n            <td class="cell" id="2D"></td>/n            <td class="cell" id="2E"></td>/n            <td class="cell" id="2F"></td>/n            <td class="cell" id="2G"></td>/n            <td class="cell" id="2H"></td>/n            <td>2</td>/n        </tr>'
-nextline = help.replace('/n','\n')
-print(nextline.replace('2','8'))
-
